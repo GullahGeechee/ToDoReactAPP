86768d5b3c3c9dde82f5837ee5f5dd508f42a2ca
Raven <user@users-MacBook-Air.local>
2022-07-05 15:45:56 -0400

    final

---
+++ Data/todosData.js
@@ -0,0 +1,17 @@
+const toDosData=[
+    {
+    tasksName:"Water Plants" , 
+    description:"provide fern, pythos and flowers with 1/3 liter of water daily" ,
+    completed: true
+    },
+    {
+        tasksName:"Empty trash" , 
+        description:"Take trash to the cuve and refill bag" ,
+        completed: false
+    },
+    {
+        tasksName:"Fold laundry" , 
+        description:"get laundry from dryer fold them, and put them away " ,
+        completed: true
+    },
+    ]
